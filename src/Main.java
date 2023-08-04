7538cb1671f97152137da01e544c594d51aa012b
Juhaina-Ali <133102246+Juhaina-Ali@users.noreply.github.com>
2023-08-04 18:44:56 +0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        double width = 5.5, height = 8.5, area, p;
+        area = height * width;
+        p = 2 *(height + width);
+        System.out.println("Area= "+area);
+        System.out.println("Parameter"+p);
+        
+    }
+}
